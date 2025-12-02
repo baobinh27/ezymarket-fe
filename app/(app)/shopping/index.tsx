3e47aba7b45c67780f7bdd380f9ade64faa7d132
skullick <Tien.NM225099@sis.hust.edu.vn>
2025-12-02 09:10:00 +0700

    add shoppinglist mock page

--- app/(app)/shopping/index.tsx
+++ app/(app)/shopping/index.tsx
@@ -1,12 +1,58 @@
-import { Text, View } from "react-native";
+import IButton from "@/components/IButton";
+import ShoppingList from "@/components/ShoppingList";
+import { CardGroup, ItemCard, IText } from "@/components/styled";
+import { Octicons } from "@expo/vector-icons";
+import React from "react";
+import { ScrollView, View } from "react-native";
 
+const shoppingLists = [
+{ id: "1", name: "Weekly Groceries" },
+{ id: "2", name: "Dinner Party" },
+{ id: "3", name: "Breakfast Items" },
+{ id: "4", name: "Pantry Staples" },
+{ id: "5", name: "Weekly Groceries" },
+{ id: "6", name: "Dinner Party" },
+{ id: "7", name: "Breakfast Items" },
+{ id: "8", name: "Pantry Staples" },
+];
 
 export default function ShoppingScreen() {
-    return (<View>
-        <Text>Shopping lists</Text>
-        <Text>Shopping lists</Text>
-        <Text>Shopping lists</Text>
-        <Text>Shopping lists</Text>
-        <Text></Text>
-    </View>)
+      return (
+      
+        <ScrollView
+            contentContainerStyle={{
+                padding: 16,
+                gap: 16
+            }}
+        >
+            <View style={{
+                flexDirection: "row",
+                gap: 8
+            }}>
+                 <IButton variant="secondary" style={{ borderRadius: 10, paddingVertical: 10, flex: 1.8 }} onPress={() => {}}>
+                    <IText color="#46982D" semiBold>Search</IText>
+                </IButton>
+                <IButton variant="primary" style={{ borderRadius: 10, paddingVertical: 16, paddingHorizontal: 12, flex: 1, flexDirection:"row", alignItems: "center", justifyContent: "space-evenly"}} onPress={() => {}}>
+                    <View style={{backgroundColor: "white", padding: 5, borderRadius: 4}}><Octicons size={24} name="plus" color="#46982D" /></View>
+                    <IText color="white" semiBold>Create</IText>
+                </IButton>
+            </View>
+
+
+
+            <ItemCard primary>
+                <ShoppingList id={"0"} name={"My shopping list"} active />
+            </ItemCard>
+
+            <CardGroup>
+                {
+                    shoppingLists.map((item) => {
+                        return <ItemCard key={item.id}><ShoppingList  id={item.id} name={item.name} /></ItemCard>
+                    })
+                }
+            </CardGroup>
+        </ScrollView>
+    );
 }
+   
+   
